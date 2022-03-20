5eab08a02a628a08f8f7d20c1b43b6b7cd3bf76f
Mikhail Bootko <m14ai1@yandex.ru>
2022-03-20 06:52:06 +0300

    validation pristine
    maybe all
    
    fix scale

--- js/upload-img-form.js
+++ js/upload-img-form.js
@@ -1,102 +1,102 @@
-const submit = document.querySelector('.img-upload__submit');
-const hashtag = document.querySelector('.text__hashtags');
+import {closeModal} from './utils.js';
 
-const showErrorMessage = () => {
-  if (submit.disabled === true && document.querySelector('.text-error-message') === null) {
-    const errorMessage = document.createElement('span');
-    errorMessage.textContent = 'Введенное значение не корректно';
-    errorMessage.style.color = '#ffffff';
-    errorMessage.classList.add('text-error-message');
-    document.querySelector('.img-upload__text').before(errorMessage);
-  } else if (submit.disabled === false && document.querySelector('.text-error-message') !== null) {
-    document.querySelector('.text-error-message').remove();
+const hashtag = document.querySelector('.text__hashtags');
+const description = document.querySelector('.text__description');
+const button = document.querySelector('.img-upload__cancel');
+const overlay = document.querySelector('.img-upload__overlay');
+const uploadForm = document.querySelector('.img-upload__form');
+const scale = document.querySelector('.img-upload__scale');
+
+//Validation
+const MAX_DESCRIPTION = 140;
+const MAX_HASHTAGS = 5;
+
+const pristine = new Pristine(uploadForm, {
+  classTo: 'img-upload__item',
+  errorClass: 'img-upload__item--invalid',
+  successClass: 'img-upload__item--valid',
+  errorTextParent: 'img-upload__item',
+  errorTextTag: 'div',
+  errorTextClass: 'img-upload__error'
+});
+
+const getHashtags = (str) => {
+  if (str) {
+    const hashtags = str.split(/\s+/g);
+    if (hashtags[0] === ''){hashtags.shift();}
+    if (hashtags[hashtags.length - 1] === '') {hashtags.pop();}
+    return hashtags;
   }
 };
 
 const validateStr = (str) => {
-  if (str[0] === '#' && str.length > 1 && str.length < 20 && str.slice(1).search(/[^\w]/g) === -1) {
-    submit.disabled = false;
-  } else {
-    submit.disabled = true;
+  if (str.length === 0) {return true;}
+  const re = /^#[A-Za-zА-Яа-яЕё0-9]{1,19}$/;
+  const hashtags = getHashtags(str);
+  const results = [];
+  hashtags.forEach((element) => {
+    results.push(re.test(element));
+  });
+  return results.indexOf(false) === -1;
+};
+
+pristine.addValidator(hashtag, validateStr, 'Какой то неправильный ХэшТег');
+
+const validateDuplicateHashtag = (value) => {
+  if (value.length === 0) {return true;}
+  const hashtags = getHashtags(value);
+  const swapArr = [...new Set(hashtags)];
+  return hashtags.length === swapArr.length;
+};
+
+pristine.addValidator(hashtag, validateDuplicateHashtag, 'Такой ХэшТег уже есть');
+
+const validationMaxCountHashTags = (value) => {
+  if (value.length === 0) {return true;}
+  const hashtags = getHashtags(value);
+  return hashtags.length < MAX_HASHTAGS;
+};
+
+pristine.addValidator(hashtag, validationMaxCountHashTags, `Максимум ${MAX_HASHTAGS} ХешТегов`);
+
+const validateDescription = (value) => value.length <= MAX_DESCRIPTION;
+
+pristine.addValidator(description, validateDescription, `Максимумум ${MAX_DESCRIPTION} символов`);
+
+//Img transformation
+const controlSmaller = scale.querySelector('.scale__control--smaller');
+const controlBigger = scale.querySelector('.scale__control--bigger');
+const scaleValue = scale.querySelector('.scale__control--value');
+
+const SCALE_STEP = 25;
+const MIN_SCALE_VALUE = 25;
+const MAX_SCALE_VALUE = 100;
+const DEFAULT_SCALE_VALUE = 100;
+
+const scaleControlHandler = (evt) => {
+  const value = parseInt(scaleValue.value, 10);
+  if (evt.target === controlSmaller) {
+    scaleValue.value = value > MIN_SCALE_VALUE ? `${value - SCALE_STEP}%` : `${MIN_SCALE_VALUE}%`;
+  }
+  if (evt.target === controlBigger) {
+    scaleValue.value = value < MAX_SCALE_VALUE ? `${value + SCALE_STEP}%` : `${MAX_SCALE_VALUE}%`;
   }
 };
 
-const findDuplicateHashtag = (arr) => {
-  const swapArr = [...new Set(arr)];
-  return arr.length === swapArr.length;
-};
-
-const HashtagValidateHandler = () => {
-  const hashtags = hashtag.value.split(/\s+/gm);
-  if (hashtags.length === 1 && hashtags[0] === '') {
-    submit.disabled = false;
-  } else {
-    submit.disabled = true;
-  }
-  if (hashtags.indexOf('') === -1 && hashtags.length <= 5 && findDuplicateHashtag(hashtags)) {
-    for (let i = 0; i < hashtags.length; i++) {
-      validateStr(hashtags[i]);
-    }
-  }
-  showErrorMessage();
-};
-const validateHashtag = () => {
-  hashtag.addEventListener('input', HashtagValidateHandler, true);
-};
-
-const description = document.querySelector('.text__description');
-
-const descriptionValidateHandler = () => {
-  showErrorMessage();
-  submit.disabled = description.value.length > 140;
-  showErrorMessage();
-};
-
-const validateDescription = () => {
-  description.addEventListener('input', descriptionValidateHandler, true);
-};
-
-const closeOverlay = (overlay, btnClass) => {
-  const closeModalHandler = (evt) => {
-    const hashtags = document.querySelector('.text__hashtags');
-
-    const closeModal = () => {
-      hashtag.value = '';
-      description.value = '';
-      document.querySelector(`.${overlay}`).classList.add('hidden');
-      document.querySelector('body').classList.remove('modal-open');
-      document.querySelector('.text__hashtags').removeEventListener('input', HashtagValidateHandler);
-      document.querySelector('.text__description').removeEventListener('input', descriptionValidateHandler);
-      document.querySelector(`.${btnClass}`).removeEventListener('click', closeModalHandler);
-      document.removeEventListener('keyup', closeModalHandler);
-    };
-
-    if (evt.target !== description && evt.target !== hashtags) {
-      if (evt.key === 'Escape') {
-        closeModal();
-      }
-    }
-    if (evt.pointerId === 1) {
-      closeModal();
-    }
-  };
-
-  document.addEventListener('keyup', closeModalHandler);
-
-  document.querySelector(`.${btnClass}`).addEventListener('click', closeModalHandler);
-};
+uploadForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
 
 const loadImg = () => {
   const loadPicture = document.querySelector('#upload-file');
   loadPicture.addEventListener('change', () => {
     document.querySelector('.img-upload__overlay').classList.remove('hidden');
     document.querySelector('body').classList.add('modal-open');
-
-    closeOverlay('img-upload__overlay', 'img-upload__cancel');
+    scaleValue.value = `${DEFAULT_SCALE_VALUE}%`;
+    scale.addEventListener('click', scaleControlHandler);
+    closeModal(button, overlay);
   });
-
-  validateHashtag();
-  validateDescription();
 };
 
-export {loadImg};
+export {loadImg, scaleControlHandler};
