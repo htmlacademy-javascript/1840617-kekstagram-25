e19e4f18d2294cf7486001b59e947ab7ba3799b2
Keks <keks@htmlacademy.ru>
2022-03-22 19:43:00 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/data.js
@@ -0,0 +1,58 @@
+import {getRandomInt} from './utils.js';
+import {shuffleArray} from './utils.js';
+
+const USERS_COUNT = 25;
+const commentsLimits = {
+  min: 1,
+  max: 10,
+};
+const likesLimits = {
+  min: 15,
+  max: 300,
+};
+const commentsArr = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+  'Very well!!!',
+  'So CooooooooL!!!!!!!!!',
+  'More comments',
+  'WTF?'
+];
+const commentatorArr = ['Алена', 'Наталья', 'Роман', 'Оксана', 'Василий', 'Борис', 'Sam', 'Евграф', 'Сигизмунд', 'Ольга', 'Kat'];
+const descriptionText = ['Редкий кадр', 'Тесты камеры', 'Сегодня солнечно', 'Ставьте лайки', 'Больше лайков', 'Какой замечательный вид'];
+
+const getComments = (number) => {
+  const commentsArray = [];
+  for (let i = 0; i < number; i++) {
+    const comment = {};
+    const randomCommentator = getRandomInt(0, (commentatorArr.length - 1));
+    const randdomMessage = getRandomInt(0, (commentsArr.length - 1));
+    comment.id = getRandomInt(50, 233);
+    comment.avatar = `img/avatar-${getRandomInt(1, 6)}.svg`;
+    comment.message = commentsArr[randdomMessage];
+    comment.name = commentatorArr[randomCommentator];
+    commentsArray.push(comment);
+  }
+  return commentsArray;
+};
+
+const getGaleryArray = function () {
+  const postsArray = [];
+  const photoNumbers = shuffleArray(Array.from({length: USERS_COUNT}, (item, index) => index + 1));
+  for (let i = 0; i < USERS_COUNT; i++) {
+    postsArray[i] = {};
+    const post = postsArray[i];
+    post.id = i + 1;
+    post.url = `photos/${photoNumbers[i]}.jpg`;
+    post.description = descriptionText[getRandomInt(0, descriptionText.length -1)];
+    post.likes = getRandomInt(likesLimits.min, likesLimits.max);
+    post.comments = getComments(getRandomInt(commentsLimits.min, commentsLimits.max));
+  }
+  return postsArray;
+};
+
+export {getGaleryArray};
---
+++ 8/js/galery.js
@@ -0,0 +1,35 @@
+import {getGaleryArray} from './data.js';
+import {showBigPicture} from './big-picture.js';
+import {showComments} from './big-picture.js';
+
+const previewList = document.querySelector('.pictures');
+const previewTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const listFragment = document.createDocumentFragment();
+
+const renderGalery = () => {
+
+  const createPreviewPicture = (pictureInfo) => {
+    const previewElement = previewTemplate.cloneNode(true);
+    previewElement.querySelector('.picture__img').src = pictureInfo.url;
+    previewElement.querySelector('.picture__comments').textContent = pictureInfo.comments.length;
+    previewElement.querySelector('.picture__likes').textContent = pictureInfo.likes;
+    previewElement.addEventListener('click', () => {
+      showBigPicture(pictureInfo);
+      showComments();
+    });
+    listFragment.appendChild(previewElement);
+  };
+
+  const createGalery = (infoArray) => {
+    infoArray.forEach((infoObj) => {
+      createPreviewPicture(infoObj);
+    });
+    previewList.appendChild(listFragment);
+  };
+
+  const photoData = getGaleryArray();
+  createGalery(photoData);
+};
+
+export {renderGalery};
